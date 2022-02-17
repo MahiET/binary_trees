fa1a0c73aca6c1e4338e3132cc50b64c24347e89
MahiET <meazalemma2020@gmail.com>
2022-02-17 01:13:27 -0800

    In-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that creates a binary tree node
- * @tree: pointer to the parent node of the node to create
- * @func: pointer of function
- * Return: pointer to the new node, or NULL on failure
+ * binary_tree_inorder - traverses a tree in in-order calling func
+ * @tree: const pointer to root node to begin traversal at
+ * @func: pointer to a function to call for each node.
+ * Return: Nothing.
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
