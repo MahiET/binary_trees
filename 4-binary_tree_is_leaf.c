c09ab77c68ba6be92b16b355accb976ea17f8f69
MahiET <meazalemma2020@gmail.com>
2022-02-17 00:59:31 -0800

    Tree is Leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - function that creates a binary tree node
- * @node: pointer to the parent node of the node to create
- * Return: pointer to the new node, or NULL on failure
+ * binary_tree_is_leaf - function that chrck if a node is a leaf
+ * @node: pointer to the parent node of the check
+ * Return: 1 if node is a leaf, otherwids NULL
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
