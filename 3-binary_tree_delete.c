8a91e00e3492379853f41cfd1e001699c439143a
MahiET <meazalemma2020@gmail.com>
2022-02-17 00:43:01 -0800

    Tree Delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,12 +2,12 @@
 
 /**
  * binary_tree_delete - function that deletes an entire binary tree
- * @tree:  pointer to the node of the parent delete
- * Return: Nothing
+ * @tree: Pointer to the node of the parent delete
+ * Returns: Nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-      if (!tree)
+	if (!tree)
 		return;
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
