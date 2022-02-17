7518aa77dbc906fa8a863f86721ecf899c956ff5
MahiET <meazalemma2020@gmail.com>
2022-02-17 02:54:47 -0800

    Binary Tree Size

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,7 +12,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL || (tree->parent == NULL))
 		return (0);
 
-	depth = binary_tree_depth(tree->parent);
+	depth += 1 + binary_tree_depth(tree->parent);
 
-	return (depth + 1);
+	return (depth);
 }
