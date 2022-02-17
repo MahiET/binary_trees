bec2af698b510b86acec9c12bc6fd9cc1ffef46b
MahiET <meazalemma2020@gmail.com>
2022-02-17 00:35:52 -0800

    Binary Tree Delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,11 +7,9 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
+      if (!tree)
 		return;
-	if (tree->left)
-	        binary_tree_delete(tree->left);
-	if (tree->right)
-	        binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 	free(tree);
 }
