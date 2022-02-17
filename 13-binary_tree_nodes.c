4a6525a56372a201eddd9002a229d4776319f8ee
MahiET <meazalemma2020@gmail.com>
2022-02-17 03:21:37 -0800

    Bineary Tree Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,20 +4,18 @@
 /**
  * binary_tree_nodes - counts the nodes with at least 1 child
  * @tree: pointer to the root node of the tree
- * Return: count
+ * Return:  Number the leaves in a binary tree
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	int left, right, count;
+       size_t nodes = 0;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	left = binary_tree_nodes(tree->left);
-	right = binary_tree_nodes(tree->right);
-
-	count = left + right + 1;
-	return (count);
+	if (tree->left || tree->right)
+		nodes += 1;
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+	return (nodes);
 }
